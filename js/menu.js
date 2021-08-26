2491bd6fa4fd0cd943a9c979c00bd4b55b5fab6c
pameg <pamegumach@gmail.com>
2021-08-26 02:30:37 +0200

    Version 4. -> Finished file: index.html and style.css               Fixed the footer.               Fixed the menu with JS

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,18 @@ const menuNavegation = document.querySelector('.menu-navegation');
 console.log(menu);
 console.log(menuNavegation);
 
+menu.addEventListener('click', ()=>{
+    //alert("Click prueba")
+    menuNavegation.classList.toggle("spread");
+})
+
+//Para que el menu aparezca una vez abiero
+//Usando funcion de flecha
+window.addEventListener('click', e=>{
+    //Para saber donde esto haciendo click?
+    //console.log(e.target);
+    if (menuNavegation.classList.contains('spread') && e.target != menu && e.target != menu){
+        menuNavegation.classList.toggle("spread");
+
+    }
+})
